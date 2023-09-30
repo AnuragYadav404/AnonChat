760f8f116a03975806a9be23407f80b50c127c7d
starboy <124185505+new-Begginings@users.noreply.github.com>
2023-09-30 12:40:56 +0530

    Implement member_form.pug and memberSignupController.signup_get

--- controllers/memberSignupController.js
+++ controllers/memberSignupController.js
@@ -1,5 +1,8 @@
 exports.memberSignup_get = function (req, res, next) {
-  res.send("This page implements member sign up get");
+  // here it has the simple thing of handling of display memeber form
+  res.render("member_form", {
+    title: "Sign up as a member now!",
+  });
 };
 
 exports.memberSignup_post = function (req, res, next) {
